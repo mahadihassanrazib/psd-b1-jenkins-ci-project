eba48078ebe021d44e377afc69cc6c790e1ea3a1
Md. Mahadi Hassan Razib <hassan.razib@bjitgroup.com>
2024-08-29 09:18:06 +0600

    first init commit

---
+++ src/main/java/psd_b1_ci/jenkin_ci_pipeline/JenkinCiPipelineApplication.java
@@ -0,0 +1,13 @@
+package psd_b1_ci.jenkin_ci_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinCiPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinCiPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/psd_b1_ci/jenkin_ci_pipeline/JenkinCiPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package psd_b1_ci.jenkin_ci_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinCiPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
