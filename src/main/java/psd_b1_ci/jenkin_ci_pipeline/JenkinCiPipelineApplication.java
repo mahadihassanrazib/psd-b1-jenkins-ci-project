acc2c20ca22d5f82e9b491ff20263436bef47ea1
Borhan Uddin <borhan.uddin@bjitgroup.com>
2024-08-29 12:21:19 +0600

    test changes 02

--- src/main/java/psd_b1_ci/jenkin_ci_pipeline/JenkinCiPipelineApplication.java
+++ src/main/java/psd_b1_ci/jenkin_ci_pipeline/JenkinCiPipelineApplication.java
@@ -1,6 +1,5 @@
 package psd_b1_ci.jenkin_ci_pipeline;
 
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +7,10 @@ import java.util.logging.Logger;
 
 @SpringBootApplication
 public class JenkinCiPipelineApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinCiPipelineApplication.class, args);
 		System.out.println("start");
+		System.out.println("Test 02");
 	}
 
 }
