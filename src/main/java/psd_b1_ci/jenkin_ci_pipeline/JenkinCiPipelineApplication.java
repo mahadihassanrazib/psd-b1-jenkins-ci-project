e35742ebaff3bb538d94ce32982a48e03058e7b2
Borhan Uddin <borhan.uddin@bjitgroup.com>
2024-08-29 11:57:21 +0600

    init from borhan 11464

--- src/main/java/psd_b1_ci/jenkin_ci_pipeline/JenkinCiPipelineApplication.java
+++ src/main/java/psd_b1_ci/jenkin_ci_pipeline/JenkinCiPipelineApplication.java
@@ -8,6 +8,7 @@ public class JenkinCiPipelineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinCiPipelineApplication.class, args);
+		System.out.println("start");
 	}
 
 }
