9aa0fdee3695221e4016890cd01368d2d917e9b8
Md. Mahadi Hassan Razib <hassan.razib@bjitgroup.com>
2024-08-29 15:48:02 +0600

    v1 addec

--- src/main/java/psd_b1_ci/jenkin_ci_pipeline/JenkinCiPipelineApplication.java
+++ src/main/java/psd_b1_ci/jenkin_ci_pipeline/JenkinCiPipelineApplication.java
@@ -6,7 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JenkinCiPipelineApplication {
 
+	static Logger logger = new LoggerFactory.getLogger(JenkinCiPipelineApplication.class)
+
 	public static void main(String[] args) {
+
+		logger.info("This is a test running job v1.")
+
 		SpringApplication.run(JenkinCiPipelineApplication.class, args);
 	}
 
